d2518d2ce85983fedd50fb9481cacbb4b1cedbee
santiago <sotelosantiago88@gmail.com>
2025-10-25 09:42:25 -0300

    wmsService: ft extension repositories transacations.

---
+++ services/WmsService/Infrastructure/Extensions/RepositoryRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WmsService.Infrastructure.Database;
+using WmsService.Infrastructure.Repositories;
+
+namespace WmsService.Infrastructure.Extensions;
+
+public static class RepositoryRegistration
+{
+    public static IServiceCollection AddRepository<TModel, TDomain>(
+        this IServiceCollection services,
+        Func<TModel, TDomain> toDomain,
+        Func<TDomain, TModel> toModel)
+        where TModel : class
+        where TDomain : class
+    {
+        services.AddScoped<IRepository<TModel, TDomain>>(sp =>
+        {
+
+            DbContainer context = sp.GetRequiredService<DbContainer>();
+            return new Repository<TModel, TDomain>(context, toDomain, toModel);
+        });
+
+        return services;
+    }
+}
