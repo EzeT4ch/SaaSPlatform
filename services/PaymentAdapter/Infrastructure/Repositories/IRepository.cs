6da6f6a9f425db029fa642d40cd48d14539c0415
santiago <sotelosantiago88@gmail.com>
2025-10-25 09:57:40 -0300

    paymentadapter: ft : unitofwork repository queryable

---
+++ services/PaymentAdapter/Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+
+namespace PaymentAdapter.Infrastructure.Repositories;
+
+public interface IRepository<TModel, TDomain> where TDomain : class where TModel : class
+{
+    Task<TDomain?> GetByIdAsync(int id, Func<IQueryable<TModel>, IQueryable<TModel>>? include = null,
+        bool isTracking = false, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<TDomain>> GetAllAsync(CancellationToken cancellationToken = default,
+        int take = 100, int page = 1, Func<IQueryable<TModel>, IQueryable<TModel>>? include = null,
+        bool isTracking = false);
+
+    Task<IEnumerable<TDomain>> FindAsync(Expression<Func<TModel, bool>> predicate,
+        Func<IQueryable<TModel>, IQueryable<TModel>>? include = null,
+        bool isTracking = false,
+        CancellationToken cancellationToken = default);
+
+    Task AddAsync(TDomain entity, CancellationToken cancellationToken = default);
+
+    Task AddRangeAsync(IEnumerable<TDomain> entities, CancellationToken cancellationToken = default);
+
+    void Update(TDomain entity, CancellationToken cancellationToken = default);
+
+    void Remove(TDomain entity, CancellationToken cancellationToken = default);
+
+    void RemoveRange(IEnumerable<TDomain> entities);
+
+    IQueryable<TModel> AsQueryable(bool trackChanges = false);
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
