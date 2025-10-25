63e10932c6f0176a3d6e45eaf73777ba00da65cd
santiago <sotelosantiago88@gmail.com>
2025-10-25 10:32:50 -0300

    .

--- services/PaymentAdapter/Infrastructure/Repositories/Repository.cs
+++ services/PaymentAdapter/Infrastructure/Repositories/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PaymentAdapter.Infrastructure.Database;
+using PaymentAdapter.Infrastructure.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
