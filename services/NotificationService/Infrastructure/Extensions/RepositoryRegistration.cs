04c5e0dc7771147d136c50e177525db8f3a0be46
santiago <sotelosantiago88@gmail.com>
2025-10-25 10:31:45 -0300

    notificationService: ft: extension repositros unitofwork

---
+++ services/NotificationService/Infrastructure/Extensions/RepositoryRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NotificationService.Infrastructure.Database;
+using NotificationService.Infrastructure.Repositories;
+
+namespace NotificationService.Infrastructure.Extensions;
+
+public static class RepositoryRegistration
+{
+    public static IServiceCollection AddRepository<TModel, TDomain>(
+        this IServiceCollection services,
+        Func<TModel, TDomain> toDomain,
+        Func<TDomain, TModel> toModel)
+        where TModel : class
+        where TDomain : class
+    {
+        services.AddScoped<IRepository<TModel, TDomain>>(sp =>
+        {
+            DbContainer context = sp.GetRequiredService<DbContainer>();
+            return new Repository<TModel, TDomain>(context, toDomain, toModel);
+        });
+
+        return services;
+    }
+}
